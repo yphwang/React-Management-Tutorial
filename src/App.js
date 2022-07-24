51c4b1a53f90329b7bcfd91bdf5b743577dbcfc6
yphwang <hyp6511@naver.com>
2022-07-24 10:42:44 +0900

    다중 고객 정보 List

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2> Let's develop management system ! </h2>
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday' : '660326',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name': '류관순',
+    'birthday' : '010326',
+    'gender' : '여자',
+    'job' : '독립운동가'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday' : '110326',
+    'gender' : '남자',
+    'job' : '해군사령관'
+  }
+]
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
